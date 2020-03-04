9740b4c43306b5e2a5865a8ef4c2646861237162
Hordent <hordydmitry@gmail.com>
2020-03-04 19:56:18 +0500

    HW1 is done

---
+++ DmitryBachurin/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const messages = [
+    { name: "Ivan", content: "Hello, world!" },
+    { name: "Petr", content: "Helo, how are you?" },
+    { name: "Ivan", content: "I'm well" },
+]
+
+
+const Message = ({name, content}) => {
+    return <li><strong>{name}:</strong> {content}</li>
+}
+
+const MessageList = ({messages}) => {
+    return (<ul>
+        {messages.map((item, index) => <Message {...item} key={index} />)}
+    </ul>);
+}
+
+ReactDOM.render(<MessageList messages={messages} />, document.getElementById("root"))
