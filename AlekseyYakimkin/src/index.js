ae53da749426ef06c92cdf01960a2fc81a58543a
lehanich <ayakimkin@gmail.com>
2020-03-05 22:15:49 +0300

    Новое сообщение

--- AlekseyYakimkin/src/index.js
+++ AlekseyYakimkin/src/index.js
@@ -1,4 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render("test", document.getElementById("root"))
+const messages = [
+    { name: "Ivan", content: "Hello, world!" },
+    { name: "Petr", content: "Helo, how are you?" },
+    { name: "Ivan", content: "I'm well" },
+]
+
+const newMessage = {name:"Alex", content: "Hi there!"}
+
+const Message = ({name, content}) => {
+    return <li><strong>{name}:</strong> {content}</li>
+}
+
+const MessageList = ({messages}) => {
+    function pushNewMessage(e){
+        console.log('Click');
+        messages.push(newMessage)
+        ReactDOM.render(<MessageList messages={messages} /> , document.getElementById("root"))
+    }
+    return (
+        <div>
+            <ul>
+                {messages.map((item, index) => <Message {...item} key={index} />)}
+            </ul>
+            <button id="newmessage" name="newmessage" val="newmessage" onClick={pushNewMessage}>Новое сообщение</button>
+        </div>
+    );
+}
+
+ReactDOM.render(<MessageList messages={messages} /> , document.getElementById("root"))
+
