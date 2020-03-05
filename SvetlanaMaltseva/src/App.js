cc5a96954a45fea600d1b105a5d3445057cfdb83
Roguey-84 <msvetlanae@mail.ru>
2020-03-05 23:22:07 +0500

    Customization is done

---
+++ SvetlanaMaltseva/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+function App() {
+  return <div className='container'>Hello world</div>;
+}
+
+export default App;
---
+++ SvetlanaMaltseva/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env', '@babel/react']
+        }
+      },
+      {
+        test: /\.(css)$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    })
+  ]
+};
