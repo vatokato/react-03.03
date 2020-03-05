54806eff61938d03afac59ab029aa791158a2d6e
jackson070896 <jackson070896@yandex.ru>
2020-03-06 01:32:50 +0700

    1 HW 06.03.2020

---
+++ DmitryVoronin/src/components/SendButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class SendButton extends React.Component {
+    render() {
+        const { text } = this.props;
+        return (
+            <button className="content__submit" type="submit">
+                {text}
+            </button>
+        );
+    }
+}
