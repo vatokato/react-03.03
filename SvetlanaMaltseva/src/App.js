cf0d581b29f95df2e57bfdecf56e4cf3b3b50386
Roguey-84 <msvetlanae@mail.ru>
2020-03-06 07:12:38 +0500

    Send Form added

--- SvetlanaMaltseva/src/App.js
+++ SvetlanaMaltseva/src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import Messageblock from './components/layout/Messageblock';
+import Usersblock from './components/layout/Usersblock';
 
-function App() {
-  return <div className='container'>Hello world</div>;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [
+        {
+          name: 'Brandon',
+          text: 'Hi, everyone! How are you?'
+        },
+        {
+          name: 'Mary',
+          text: "Hi, Brandon. I'm fine, thanks. And you?"
+        },
+        {
+          name: 'John',
+          text: 'Hey, Brandon. Long time no see.'
+        }
+      ]
+    };
+    this.addMessage = this.addMessage.bind(this);
+  }
+  addMessage(message) {
+    const newMessageList = [...this.state.messages, message];
+    this.setState({ messages: newMessageList });
+  }
+
+  render() {
+    const { messages } = this.state;
+    return (
+      <div>
+        <div className='container-fluid'>
+          <div className='row'>
+            <Usersblock messages={messages} />
+            <Messageblock messages={messages} addMessage={this.addMessage} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
