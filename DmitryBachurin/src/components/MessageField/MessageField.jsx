98154712d5aa5fec52bdda11a2bcef8d47a2e514
hordydmitry <hordydmitry@gmail.com>
2020-03-09 23:26:18 +0500

    HW finished

--- DmitryBachurin/src/components/MessageField/MessageField.jsx
+++ DmitryBachurin/src/components/MessageField/MessageField.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Message} from './Message/Message'
+import { Message } from './Message/Message'
 
 export default class MessegeField extends Component {
     state = {
@@ -9,13 +9,50 @@ export default class MessegeField extends Component {
             { name: "Ivan", content: "I'm well" },
         ]
     }
-    render() {
-        const { state: { messages } } = this;
-        return (
 
-            <ul>
-                {messages.map((item, index) => <Message {...item} key={index} />)}
-            </ul>
+
+    handleClick = (e) => {
+        e.preventDefault();
+        const { name, message } = e.target;
+
+        Promise.resolve()
+            .then(
+                this.setState(
+                    (state) => (state.messages.push({ name: name.value, content: message.value }))
+                )
+            )
+            .then(
+                this.setState(
+                    (state) => (state.messages.push({ name: 'Robot', content: `Уважаемый ${name.value}! Ваше сообщение принято.` }))
+                )
+            )
+            .catch(err => console.log(err));
+
+    }
+    render() {
+        const { state: { messages }, handleClick } = this;
+        return (
+            <>
+                <ul>
+                    {messages.map((item, index) => <Message {...item} key={index} />)}
+                </ul>
+
+                <form onSubmit={handleClick}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Введите имя"
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="message"
+                        placeholder="Введите сообщение"
+                        required
+                    />
+                    <button type="submit">Отправить сообщение</button>
+                </form>
+            </>
         )
     }
 }
