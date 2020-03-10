eb3fdfa5a5b91bf94352e91157c10be31170447a
Oleg Shikov <ol.shikov@yandex.ru>
2020-03-10 21:57:44 +0300

    Lesson 3

--- OlegShikov/src/components/Message/Message.jsx
+++ OlegShikov/src/components/Message/Message.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classname from 'classname';
+
+import './Message.css';
 
 export const Message = ({name, content}) => {
-    return <li><strong>{name}:</strong> {content}</li>
+    const className = classname("Message", {'Message--robot': name === 'Robot'});
+    return <li className={className} ><strong>{name}:</strong> {content}</li>
+}
+
+Message.propTypes = {
+    name: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
 }
