e01ea1c0cfdc58698f9258cdc9aff6487e772562
xolab <xolab@mail.ru>
2020-03-17 00:31:29 +0300

    lesson 3

--- EvgeniiAnisimov/src/containers/ChatContainer.jsx
+++ EvgeniiAnisimov/src/containers/ChatContainer.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import { Chat } from '../components/Chat/Chat';
 
+export const ROBOT = 'Robot';
+
 export class ChatContainer extends Component {
   state = {
     messages: [
       {name: "Ivan", content: "Hello, world"},
       {name: "Petr", content: "Hello, how are you?"},
       {name: "Ivan", content: "I'm well"}
-    ]
+    ],
+    lastMessage: {name: "Ivan", content: "I'm well"}
+  }
+
+  componentDidUpdate() {
+    // const lastMessage = this.state.messages[this.state.messages.length - 1];
+    // console.log("componentDidUpdate->" + lastMessage.name);
+
+    this.handleRobot()
+    // if(lastMessage.name !== ROBOT) {
+    //   setTimeout(() => this.handleSendMessage({
+    //     name: ROBOT,
+    //     content: `Hello, ${lastMessage.name}, I'm Robot!`,
+    //   }), 3000)
+    // }
+  }
+
+  handleRobot = () => {
+    this.setState((state) => ({lastMessage: state.messages[state.messages.length - 1]}))
+
+    console.log("handleRobot->" + this.state.lastMessage.name)
+    // if(this.state.lastMessage.name !== 'Robot') {
+    //   setTimeout(() => this.handleSendMessage({
+    //     name: 'Robot',
+    //     content: `Hello, ${lastMessage.name}, I'm Robot!`,
+    //   }), 3000)
+    // }
   }
 
   handleSendMessage = (message) => {
