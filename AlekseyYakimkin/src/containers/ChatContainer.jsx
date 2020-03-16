839803f556d969cb4ee7f5383677eb4b838d2962
lehanich <ayakimkin@gmail.com>
2020-03-16 23:10:30 +0300

    Факультативное задание с setTimeout

--- AlekseyYakimkin/src/containers/ChatContainer.jsx
+++ AlekseyYakimkin/src/containers/ChatContainer.jsx
@@ -9,14 +9,16 @@ export class ChatContainer extends Component {
             { name: "Ivan", content: "Hello, world!" },
             { name: "Petr", content: "Helo, how are you?" },
             { name: "Ivan", content: "I'm well" },
-        ]
+        ],
+        timerId :null
     }
+    timerId = null
 
     componentDidUpdate() {
         const  lastMessage = this.state.messages[this.state.messages.length - 1]
-
+        clearInterval(this.timerId)
         if(lastMessage.name != ROBOT){
-            setTimeout(() => this.handleSendMessage({
+            this.timerId = setTimeout(() => this.handleSendMessage({
                 name: ROBOT,
                 content: `Hello ${lastMessage.name}, I'm ${ROBOT}`
                 }),1000)
@@ -24,6 +26,7 @@ export class ChatContainer extends Component {
     }
 
     handleSendMessage = (message) => {
+        console.dir(this.timerId)
         this.setState((state) => ({
             messages:[...state.messages, message]
         }))
