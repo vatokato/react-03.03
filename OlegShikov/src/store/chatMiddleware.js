7dd1f9eddb5ba66e12d079da19eb67912ce40441
Oleg Shikov <ol.shikov@yandex.ru>
2020-03-27 22:00:52 +0300

    Lesson 7

--- OlegShikov/src/store/chatMiddleware.js
+++ OlegShikov/src/store/chatMiddleware.js
@@ -1,5 +1,5 @@
-import {createChat, addChat} from './chatActions';
-import {push} from 'connected-react-router';
+import {sendMessage, addChat, fire, unfire} from './chatActions';
+import {push, LOCATION_CHANGE} from 'connected-react-router';
 
 export default store => next => action => {
     next(action);
@@ -10,6 +10,14 @@ export default store => next => action => {
     // }else 
     if(action.type === addChat.toString()){
         store.dispatch(push('/chats/' + action.payload.id));
+    }else if(action.type === sendMessage.toString()){
+        const currentChat = store.getState().router.location.pathname.split('/')[2];
+        if(currentChat !== action.payload.id) {
+            store.dispatch(fire(action.payload.id));
+        }
+    }else if(action.type === LOCATION_CHANGE) {
+        const currentChat = store.getState().router.location.pathname.split('/')[2];
+        store.dispatch(unfire(currentChat));
     }
     
 }
--- OlegShikov/src/store/chatOperations.js
+++ OlegShikov/src/store/chatOperations.js
@@ -1,7 +1,45 @@
-import {addChat} from './chatActions';
+import {loadingChats, failedLoadedChats, initChats, addChat, sendMessage} from './chatActions';
+import { v4 as uuid } from 'uuid';
+import { createAction } from 'redux-api-middleware';
 
+export const fetchChats = () => createAction({
+    endpoint: '/api/chats.json',
+    method: 'GET',
+    types: [loadingChats.toString(), initChats.toString(), failedLoadedChats()]
+});
+// export const fetchChats = () => async (dispatch) => {
+//     dispatch(loadingChats());
+//     try{
+//         const res = await fetch('/api/chats.json')
+//         if(res.status !== 200){
+//             throw new Error("Error with status " + res.status)
+//         }
+//         const data = await res.json();
+//         dispatch(initChats(data));
+//     }catch(e) {
+//         dispatch(failedLoadedChats(e.message))
+//     }
+    
+// };
 
 export const createChat = (name) => (dispatch, getState) => {
     const id = Date.now();
-    dispatch(addChat(id, name));
+    const botId = uuid().replace(/-/gi, '').slice(0,16);
+    console.log("createChat -> botId", botId)
+    dispatch(addChat(id, name, botId));
+}
+
+export const sendMessageToBot = (botName, id, content) => async (dispatch, getState) => {
+    const userId = getState().chats.chats[id].botId;
+    const formData = new FormData;
+    formData.append('query', JSON.stringify({
+        userid: userId,
+        ask: content,
+    }));
+
+    const res = await fetch('/bot/', {method: 'POST', body: formData});
+    const {aiml} = await res.json();
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    dispatch(sendMessage(id, botName, aiml))
+    
 }
